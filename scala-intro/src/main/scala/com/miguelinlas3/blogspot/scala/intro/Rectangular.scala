1214420cd423050d49689a68fd616143c4312159
Miguel Pastor <miguel.pastor@liferay.com>
2011-11-07 10:46:11 +0100

    Simple trait

---
+++ scala-intro/src/main/scala/com/miguelinlas3/blogspot/scala/intro/Rectangular.scala
@@ -0,0 +1,19 @@
+package com.miguelinlas3.blogspot.scala.intro
+
+class Point(val x: Int, val y: Int)
+
+trait Rectangular {
+  def topLeft: Point
+  def bottomRight: Point
+  def left = topLeft.x
+  def right = bottomRight.x
+  def width = right - left
+
+  // más métodos
+}
+
+// mixin de la clase Component con el trait Rectangular
+
+abstract class Component extends Rectangular {
+  // nuevos métodos para este tipo de widgets
+}
